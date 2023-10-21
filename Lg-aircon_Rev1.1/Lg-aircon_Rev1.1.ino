c05c95da8f41ba16ad94d1df95405368377223b8
robsol1957 <45388524+robsol1957@users.noreply.github.com>
2023-10-21 13:08:02 +0800

    lgaircon

---
+++ Lg-aircon_Rev1.1/Lg-aircon_Rev1.1.ino
@@ -0,0 +1,181 @@
+ 
+#include <OneWire.h> 
+#include <DallasTemperature.h> //Thermocpouples
+#include <Arduino.h>
+#include "PinDefinitionsAndMore.h" // Define macros for input and output pin etc.
+#include <IRremote.hpp>
+#define INFO // Deactivate this to save program memory and suppress info output from the LG-AC driver.
+#include "ac_LG.hpp"
+
+#define IR_RX_PIN 8 //IR Receive
+#define ONE_WIRE_BUS 2 //For Thermocouples and one wire devices
+#define IR_TX_PIN 3 //IR LED - This has to be the value
+#define TEMPERATURE_PRECISION 12
+
+#define DISABLE_CODE_FOR_RECEIVER // Saves 450 bytes program memory and 269 bytes RAM if receiving functions are not used.
+
+
+
+
+
+
+OneWire oneWire(ONE_WIRE_BUS);// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+DallasTemperature sensors(&oneWire); // Pass our oneWire reference to Dallas Temperature.
+Aircondition_LG MyLG_Aircondition;
+
+
+DeviceAddress tc[5]; // arrays to hold device addresses
+int Nsensors,airconState;
+float   maxtemp = 21.5;
+float   smoothTemp;
+float   alpha=0.5;
+float   mintemp = maxtemp - 1.5;
+float   sleeptemp = maxtemp-1;
+unsigned long thistime;
+void initTCs() {
+  //////////////////////////////////////////
+  // Connect Thermocouples
+  /////////////////////////////////////////
+  // Start up the library
+  Serial.println("initialising Thermocouples.");
+  sensors.begin();
+  Nsensors = sensors.getDeviceCount();
+  Serial.print(Nsensors);
+  Serial.println(" Found");
+  // locate devices on the bus
+  oneWire.reset_search();
+  int i = 0;
+  while (oneWire.search(tc[i]) && i < Nsensors) {
+    sensors.setResolution(tc[i], TEMPERATURE_PRECISION);
+    ++i;
+  }
+}
+
+float getSmoothTemp()
+{
+  //float temp;
+  float temp=getTemp();
+  smoothTemp=smoothTemp*alpha+(1-alpha)*temp;
+//  Serial.println("    Smoothtemp ," + String(smoothTemp));
+  return(smoothTemp);
+}
+
+void writetext(String text){
+  Serial.println(",data,"+String(thistime)+","+text);
+}
+float getTemp()
+{
+  int i;
+  float result, tempC;
+  result = 0;
+  sensors.requestTemperatures();
+  for (i = 0; i < Nsensors; ++i) {
+    tempC = sensors.getTempC(tc[i]);
+//   Serial.print("Currenttemp ," + String(tempC));
+   result = result + tempC;
+  }
+  result = result / float(Nsensors);
+  return (result);
+}
+
+
+void setup() {
+  thistime = millis();
+  // Basic setup
+  //
+  Serial.begin(9600);
+  initTCs();
+  pinMode(LED_BUILTIN, OUTPUT);
+  #if defined(__AVR_ATmega32U4__) || defined(SERIAL_PORT_USBVIRTUAL) || defined(SERIAL_USB) /*stm32duino*/|| defined(USBCON) /*STM32_stm32*/|| defined(SERIALUSB_PID) || defined(ARDUINO_attiny3217)
+  delay(4000); // To be able to connect Serial monitor after reset or power up and before first print out. Do not wait for an attached Serial Monitor!
+  #endif
+  // Just to know which program is running on my Arduino
+    Serial.println(F("START " __FILE__ " from " __DATE__ "\r\nUsing library version " VERSION_IRREMOTE));
+
+    /*
+     * The IR library setup. That's all!
+     */
+  #if defined(IR_SEND_PIN)
+    IrSender.begin(); // Start with IR_SEND_PIN as send pin and enable feedback LED at default feedback LED pin
+    Serial.println(F("Ready to send IR signals at pin " STR(IR_SEND_PIN)));
+  #else
+    IrSender.begin(3, ENABLE_LED_FEEDBACK, USE_DEFAULT_FEEDBACK_LED_PIN); // Specify send pin and enable feedback LED at default feedback LED pin
+    Serial.println(F("Ready to send IR signals at pin 3"));
+  #endif
+
+    Serial.println();
+    MyLG_Aircondition.setType(LG_IS_WALL_TYPE);
+    MyLG_Aircondition.printMenu(&Serial);
+// Start running
+// Initially test to see if temp is outside of deadband and set aircon state appropriately.
+// If in side deadband then just leave alone
+ airconState=0;
+ smoothTemp=getTemp();
+ accontrol(int(2));
+}
+
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+   
+  smoothTemp=getSmoothTemp();
+  thistime = millis();
+  if(airconState == 0 ){
+    if( smoothTemp > maxtemp){
+      writetext("Off-Full,"+String(smoothTemp));
+      accontrol(int(2));
+    } else if( smoothTemp > sleeptemp) {
+        writetext("Sleep-off,"+String(smoothTemp));
+        accontrol(int(1));
+      }
+      } else if(airconState == 1) {
+        if (smoothTemp > maxtemp){
+          writetext("Sleep-Full,"+String(smoothTemp));
+          accontrol(int(2)); 
+        } else if (smoothTemp <mintemp){
+          accontrol(int(0));
+          writetext("Sleep-Off,"+String(smoothTemp));
+        }
+      } else if(airconState == 2){
+        if(smoothTemp<mintemp){
+          accontrol(int(0));
+          writetext("Full-Sleep,"+String(smoothTemp));
+        } else if(smoothTemp < sleeptemp){
+          accontrol(int(1));
+          writetext("Full-Sleep,"+String(smoothTemp));
+          
+        }
+        
+      } 
+ writetext("aircon status ,"+String(airconState));
+ writetext("Current_temp ,"+String(smoothTemp));
+ delay(5000); 
+}
+
+void accontrol(int Action) {
+//  Serial.println("In control loop "+String(Action));
+if(Action==0) {
+    Serial.println("Turning off");
+    MyLG_Aircondition.sendCommandAndParameter('0', 0);
+    airconState = 0;
+} else  if(Action==1){
+    Serial.println("Turning sleep");
+    MyLG_Aircondition.sendCommandAndParameter('1', 0);
+    delay(100);
+    MyLG_Aircondition.sendCommandAndParameter('t', mintemp);
+    delay(100);
+    MyLG_Aircondition.sendCommandAndParameter('S', 420);
+    airconState=1;
+  } else   if(Action==2){
+    Serial.println("Turning full");
+    MyLG_Aircondition.sendCommandAndParameter('C',0);
+    delay(100);
+    MyLG_Aircondition.sendCommandAndParameter('1', 0);
+    delay(100);
+    MyLG_Aircondition.sendCommandAndParameter('t', mintemp);
+    delay(100);
+    MyLG_Aircondition.sendCommandAndParameter('f', 1);
+    airconState=2;
+  }
+ }
---
+++ Nano_Darkroom_timer_3/Nano_Darkroom_timer_3.ino
@@ -0,0 +1,348 @@
+
+/////////////////////////////////////////////////////
+// Buzzer
+//////////////////////////////////////////////////////////
+#define BUZERPIN 5
+#define TOGGLEPIN 6
+//////////////////////////////////////////////////////
+// Rotary Encoder Header
+/////////////////////////////////////////////////////
+
+// https://lastminuteengineers.com/rotary-encoder-arduino-tutorial/
+#define DT 4 // Rotary Encoder
+#define CLK 3//Pin2 Rotary Encoder
+#define SW 2
+int lastStateCLK;
+
+///////////////////////////////////////////////////////////
+// LCD Header
+//////////////////////////////////////////////////////////
+/**************** LED Pin Definitions ********
+  pin 12 is connected to the DIN DataIn
+  pin 13 is connected to the CLK
+  pin 10 is connected to CS (LOAD)
+*/
+
+#define LCD_DIN 11
+#define LCD_CLK 13
+#define LCD_CS 10
+
+char __mathHelperBuffer[17];
+float stage[4];
+#include "LedControl.h"
+LedControl lc = LedControl(LCD_DIN, LCD_CLK, LCD_CS, 0);
+
+
+///////////////////////////////////////
+//
+//////////////////////////////////////
+unsigned long agitate_period = 30000;
+
+void setup() {
+  // put your setup code here, to run once:
+  // Setup Serial Monitor
+  Serial.begin(9600);
+
+  /////////////////////////////////////////////////////
+  // Set Up Rotary Encoder
+  ////////////////////////////////////////////////////
+
+  // Set encoder pins as inputs
+  pinMode(CLK, INPUT);
+  pinMode(DT, INPUT);
+  pinMode(SW, INPUT_PULLUP);
+
+ 
+
+  // Read the initial state of CLK
+  lastStateCLK = digitalRead(CLK);
+
+  /////////////////////////////////////////////////////
+  // LCD Display
+  ////////////////////////////////////////////////////
+ // pinMode(LCD_DIN, OUTPUT);
+ // pinMode(LCD_CLK, OUTPUT);
+  pinMode(LCD_CS, OUTPUT);
+
+  lc.shutdown(0, false);
+  lc.setIntensity(0, 8);
+  lc.clearDisplay(0);
+
+  /// Start of block code
+  //
+  // Get and update delay durations
+  get_delays(); // update the inputs
+  //////////////////////////////////////////////////
+  // Hang until the button pressed
+  /////////////////////////////////////////////////
+  /*
+  int was = digitalRead(TOGGLEPIN);
+  do {
+  } while (digitalRead(TOGGLEPIN) == was);
+  //Alert that buzzer has been pressed before jumping to timer.
+  //wait 30 seconds with 10 second countdown
+  wait(millis() + 30000, 10, 1000);
+  int this_event = 0;
+  unsigned long run_start = millis();
+  while (stage[this_event] > 0) {
+    unsigned long event_end = run_start + 1000 * stage[this_event];
+    unsigned long next_Agitate = millis() + agitate_period;
+    while (event_end > next_Agitate + 15000) {
+      wait(next_Agitate, 3, 1000); // three pulses just prior to next agitate
+      next_Agitate = millis() + agitate_period;
+    };
+    wait(event_end, 10, 1000);
+    run_start = event_end;
+    ++this_event;
+  }
+  pulse(100);
+  */
+  lc.shutdown(0, false);
+}
+
+void loop() {
+ lc.setIntensity(0, 0);
+
+  displayDigit_clr(300, 0, 0);
+  delay(10);
+  lc.clearDisplay(0);
+  delay(30000);
+}
+
+/////////////////////////////////////////////
+//get data
+//////////////////////////////////////////////
+
+void get_delays() {
+  int i;
+  stage[0] = get_enc_val(500, 1, SW);
+  Serial.println(stage[0]);
+  stage[1] = get_enc_val(60, 1, SW);
+  Serial.println(stage[1]);
+  stage[2] = get_enc_val(300, 1, SW);
+  Serial.println(stage[2]);
+  Serial.println("Shutting led down");
+  stage[3]=0;
+  for (i = 0; i < 4; ++i) {
+    Serial.println(stage[i]);
+    lc.clearDisplay(0);
+    delay(100);
+    displayDigit_clr(stage[i], 1, 0);
+    delay(1000);
+  }
+
+  lc.shutdown(0, true);
+}
+
+
+
+
+
+
+void wait(unsigned long thisend, int pulses, int dur) {
+  thisend = thisend - pulses * dur;
+  while (millis() < thisend) {}
+  triple(pulses, dur);
+}
+
+/////////////////////////////////////////////////
+// Buzzer utils
+//////////////////////////////////////////////////
+void pulse(unsigned long dur) {
+
+  digitalWrite(BUZERPIN, HIGH);
+  delay(dur);
+  digitalWrite(BUZERPIN, LOW);
+}
+///////////////////////////////////////////////////
+// Repeated pulse pulse times for a duration of dur
+// interspursed with a 100ms pause
+//////////////////////////////////////////////////
+void triple(int times, unsigned long dur) {
+  for (int i = 1; i <= times; ++i) {
+    pulse(100);
+    delay(dur - 100);
+  }
+}
+
+
+
+
+//////////////////////////////////////////////////////////
+// Rotary Encoder utility routines
+/////////////////////////////////////////////////////////
+float get_enc_val(float value, float factor, int button) {
+  int digs = 0;
+  displayDigit_clr(value, digs, 0);
+  while (check_button_press(button) == false) {
+    float old_value = value;
+    value = read_enc_change(value, factor);
+    if (old_value != value) {
+      Serial.print(" value: ");
+      Serial.println(value);
+      displayDigit_clr(value, digs, 0);
+    }
+  }
+  Serial.println("Button pressed");
+  delay(500);
+  return (value);
+}
+
+float read_enc_change(float value, float factor) {
+  int currentStateCLK = digitalRead(CLK);
+  if (currentStateCLK != lastStateCLK  && currentStateCLK == 1) {
+    if (digitalRead(DT) != currentStateCLK) {
+      value = value + factor;
+    } else {
+      value = value - factor;
+    }
+  }
+  lastStateCLK = currentStateCLK;
+  return (value);
+}
+bool check_button_press(int button) {
+  bool ret = true;
+  unsigned long enter = millis();
+  int btnState = digitalRead(button);
+  while (btnState == LOW && millis() < enter + 50) {
+    btnState = digitalRead(button);
+  }
+  if (millis() < enter + 50) {
+    ret = false;
+  }
+  return (ret);
+}
+/////////////////////////////////////////////////////////////////
+// LCD Utilities
+/////////////////////////////////////////////////////////////////
+/*
+   Chunk of code for converting any number into scientific notation.
+*/
+char * sci(double number, int digits)
+{
+  int exponent = 0;
+  int pos = 0;
+  // Handling these costs 13 bytes RAM
+  // shorten them with N, I, -I ?
+  if (isnan(number))
+  {
+    strcpy(__mathHelperBuffer, "nan");
+    return __mathHelperBuffer;
+  }
+  if (isinf(number))
+  {
+    if (number < 0) strcpy(__mathHelperBuffer, "-inf");
+    strcpy(__mathHelperBuffer, "inf");
+    return __mathHelperBuffer;
+  }
+  // Handle negative numbers
+  bool neg = (number < 0.0);
+  if (neg)
+  {
+    __mathHelperBuffer[pos++] = '-';
+    number = -number;
+  }
+  while (number >= 10.0)
+  {
+    number /= 10;
+    exponent++;
+  }
+  while (number < 1 && number != 0.0)
+  {
+    number *= 10;
+    exponent--;
+  }
+  // Round correctly so that print(1.999, 2) prints as "2.00"
+  double rounding = 0.5;
+  for (uint8_t i = 0; i < digits; ++i)
+  {
+    rounding *= 0.1;
+  }
+  number += rounding;
+  if (number >= 10)
+  {
+    exponent++;
+    number /= 10;
+  }
+  // Extract the integer part of the number and print it
+  uint8_t d = (uint8_t)number;
+  double remainder = number - d;
+  __mathHelperBuffer[pos++] = d + '0';   // 1 digit before decimal point
+  if (digits > 0)
+  {
+    __mathHelperBuffer[pos++] = '.';  // decimal point TODO:rvdt CONFIG?
+  }
+  // Extract digits from the remainder one at a time to prevent missing leading zero's
+  while (digits-- > 0)
+  {
+    remainder *= 10.0;
+    d = (uint8_t)remainder;
+    __mathHelperBuffer[pos++] = d + '0';
+    remainder -= d;
+  }
+  // print exponent
+  __mathHelperBuffer[pos++] = 'E';
+  neg = exponent < 0;
+  if (neg)
+  {
+    __mathHelperBuffer[pos++] = '-';
+    exponent = -exponent;
+  }
+  else __mathHelperBuffer[pos++] = '+';
+  // 3 digits for exponent;           // needed for double
+  // d = exponent / 100;
+  // __mathHelperBuffer[pos++] = d + '0';
+  // exponent -= d * 100;
+  // 2 digits for exponent
+  d = exponent / 10;
+  __mathHelperBuffer[pos++] = d + '0';
+  d = exponent - d * 10;
+  __mathHelperBuffer[pos++] = d + '0';
+  __mathHelperBuffer[pos] = '\0';
+  return __mathHelperBuffer;
+}
+/*
+  displayDigit displays a number to the display unit
+  you can set the number of digits after the decimal point
+  but if the resulting number is >8 then the number will
+  be converted to scientific notation.
+*/
+void displayDigit(double _inVal_, int _DecPoints_, int dispPos) {
+  String c; //Single character variable to send to the display module
+  int  i,val;
+  bool dpf;
+  String pt = ".";
+  String s = String(_inVal_, _DecPoints_);
+ 
+  if (s.length() > 8) { // if the number more than 8 digits convert it to scientific notation.
+    s = sci(_inVal_, 2);
+  }
+  int len = int(s.length());
+  for (i = len - 1; i >= 0; --i) { // start from the back end of the string
+    c = s.substring(i, i+1);
+
+    if ( c == pt) { /*if the character is a decimal point set the decimal point flag to true and read the character to the left of the decimal point*/
+      --i;      //Skip to the next character to the left
+      c = s[i]; // and read it
+      dpf = true;
+    } else {
+      dpf = false;
+    }
+    val=c.toInt();
+   lc.setDigit(0, dispPos, val, dpf);
+    ++dispPos;
+  }
+}
+void writetemp(double _inVal1_, double _inVal2_) {
+  lc.clearDisplay(0);
+  displayDigit( _inVal1_, 1, 0);
+  displayDigit( _inVal2_, 2, 4);
+  delay(1000);
+}
+
+void displayDigit_clr(double _inVal_, int _DecPoints_, int dispPos) {
+  lc.clearDisplay(0);
+  Serial.print("disp dig :");
+  Serial.println(_inVal_);
+  displayDigit( _inVal_, _DecPoints_, dispPos);
+}
